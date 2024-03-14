4f6c30c58d5f7596a95056e97995f4e83195030c
Ahmad Naufal Labiib Nabhaan <ahmad.nabhaan@students.amikom.ac.id>
2024-03-14 09:48:51 +0700

    debug check

--- nets/yolo.py
+++ nets/yolo.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 
-from nets.CSPdarknet53_tiny import darknet53_tiny
 from nets.mobilenet_v3 import mobilenet_v3
 from nets.attention import cbam_block, eca_block, se_block, CA_Block
 
@@ -31,7 +30,7 @@ class MobileNetV3Small(nn.Module):
 
 #-------------------------------------------------#
 #   卷积块 -> 卷积 + 标准化 + 激活函数
-#   Conv2d + BatchNormalization + LeakyReLU
+#   Conv2d + BatchNormalization + ReLU6
 #-------------------------------------------------#
 class BasicConv(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1):
@@ -39,13 +38,24 @@ class BasicConv(nn.Module):
 
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, kernel_size//2, bias=False)
         self.bn = nn.BatchNorm2d(out_channels)
-        self.activation = nn.LeakyReLU(0.1)
+        self.activation = nn.ReLU6(inplace=True)
 
     def forward(self, x):
         x = self.conv(x)
         x = self.bn(x)
         x = self.activation(x)
         return x
+
+def conv_dw(filter_in, filter_out, stride = 1):
+    return nn.Sequential(
+        nn.Conv2d(filter_in, filter_in, 3, stride, 1, groups=filter_in, bias=False),
+        nn.BatchNorm2d(filter_in),
+        nn.ReLU6(inplace=True),
+
+        nn.Conv2d(filter_in, filter_out, 1, 1, 0, bias=False),
+        nn.BatchNorm2d(filter_out),
+        nn.ReLU6(inplace=True),
+    )
 
 #---------------------------------------------------#
 #   卷积 + 上采样
@@ -68,7 +78,8 @@ class Upsample(nn.Module):
 #---------------------------------------------------#
 def yolo_head(filters_list, in_filters):
     m = nn.Sequential(
-        BasicConv(in_filters, filters_list[0], 3),
+        conv_dw(in_filters, filters_list[0]),
+        
         nn.Conv2d(filters_list[0], filters_list[1], 1),
     )
     return m
@@ -76,24 +87,21 @@ def yolo_head(filters_list, in_filters):
 #   yolo_body
 #---------------------------------------------------#
 class YoloBody(nn.Module):
-    def __init__(self, anchors_mask, num_classes, phi=0, pretrained=False, backbone="darknet"):
+    def __init__(self, anchors_mask, num_classes, phi=0, pretrained=False, backbone="mobilenetv3large"):
         super(YoloBody, self).__init__()
         self.phi            = phi
-        if backbone == "darknet":
-            self.backbone       = darknet53_tiny(pretrained)
-            in_filter           = [512, 384]
-        elif backbone == "mobilenetv3large":
+        if backbone == "mobilenetv3large":
             self.backbone       = MobileNetV3Large(pretrained=pretrained)
-            in_filter           = [160, 152]
+            in_filter           = 160
         elif backbone == "mobilenetv3small":
             self.backbone       = MobileNetV3Small(pretrained=pretrained)
-            in_filter           = [96, 64]
+            in_filter           = 96
 
-        self.conv_for_P5    = BasicConv(in_filter[0],256,1)
+        self.conv_for_P5    = BasicConv(in_filter,256,1)
         self.yolo_headP5    = yolo_head([512, len(anchors_mask[0]) * (5 + num_classes)],256)
 
         self.upsample       = Upsample(256,128)
-        self.yolo_headP4    = yolo_head([256, len(anchors_mask[1]) * (5 + num_classes)], in_filter[1])
+        self.yolo_headP4    = yolo_head([256, len(anchors_mask[1]) * (5 + num_classes)],384)
 
         if 1 <= self.phi and self.phi <= 4:
             self.feat1_att      = attention_block[self.phi - 1](256)
@@ -132,6 +140,7 @@ class YoloBody(nn.Module):
         P4 = torch.cat([P5_Upsample,feat1],axis=1)
 
         # 26,26,384 -> 26,26,256 -> 26,26,255
+        print(P4.size())
         out1 = self.yolo_headP4(P4)
         
         return out0, out1
